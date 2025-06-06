b5b485f17e9dbd5f74b4aed1409933f225953b7f
xihanM <2021307150305@cau.edu.cn>
2025-06-06 12:51:21 +0800

    sh文件

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python legged_gym/legged_gym/scripts/train.py --task=go2 --num_envs=16384 --headless --max_iterations=2000
+python legged_gym/legged_gym/scripts/train.py --task=go2 --num_envs=4096 --headless --max_iterations=10000
